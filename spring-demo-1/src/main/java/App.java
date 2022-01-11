2062206af2eb97b780e329234b98baa37e7b9103
qinfengzhu <876941171@qq.com>
2022-01-12 01:18:26 +0800

    调整pom内容让编译通过，方便后续源码调试

--- spring-demo-1/src/main/java/App.java
+++ spring-demo-1/src/main/java/App.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 class App {
     public static void main(String[] args) {
+        //不能这么做,用SpringTest更好,方便启动测试
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         FirstService firstService = context.getBean("firstService", FirstService.class);
         System.out.println(firstService.getInfo());
